03c97c3d4ba62cc2e3dbc28a8dc5ef87cefe21be
Emre Karayaman <karayaman.emre@outlook.com>
2023-06-17 01:29:52 +0300

    events and structs

--- contracts/bsc/CarRentalPlatform.sol
+++ contracts/bsc/CarRentalPlatform.sol
@@ -21,4 +21,30 @@ contract CarRentalPlatform {
     uint start;
   }
 
+  struct Car {
+    uint id;
+    string name;
+    string imgUrl;
+    Status status;
+    uint rentFee;
+    uint saleFee;
+  }
+
+  enum Status {
+    Retired,
+    InUse,
+    Available
+  }
+
+  event CarCreated(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
+  event CarMetaDataUpdated(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
+  event CarStatusUpdated(uint indexed id, Status status);
+
+  event UserCreated(address indexed walletAddress, string name, string lastName);
+  event Deposit(address indexed walletAddress, uint amount);
+  event ChechOut(address indexed walletAddress, uint indexed carId);
+  event CheckIn(address indexed walletAddress, uint indexed carId);
+  event PaymentMade(address indexed walletAddress, uint amount);
+  event Withdraw(address indexed walletAddress, uint amount);
+
 }
