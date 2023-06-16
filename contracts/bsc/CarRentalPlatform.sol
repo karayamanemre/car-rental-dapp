8d5914d5e1f8cf6218571d5d5e082f1b2b381025
Emre Karayaman <karayaman.emre@outlook.com>
2023-06-17 01:22:23 +0300

    openzeppelin contracts installed

--- contracts/bsc/CarRentalPlatform.sol
+++ contracts/bsc/CarRentalPlatform.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract CarRentalPlatform {
-  
+  using Counters for Counters.Counter;
+  Counters.Counter private _counter;
+
+  address private owner;
+
+  uint private totalPayments;
+
+  struct User {
+    address walletAddress;
+    string name;
+    string lastName;
+    uint rentedCarId;
+    uint balance;
+    uint debt;
+    uint start;
+  }
+
 }
