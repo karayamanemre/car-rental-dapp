3310aa0b813d7a7e248e204b86d4a8bd7801dd62
Emre Karayaman <karayaman.emre@outlook.com>
2023-06-17 02:31:58 +0300

    compiling

---
+++ migrations/1_deploy_car_rental_platform.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const CarRentalPlatform = artifacts.require('CarRentalPlatform');
+
+module.exports = async function (deployer) {
+  await deployer.deploy(CarRentalPlatform);
+  const deployed = await CarRentalPlatform.deployed();
+  let CarRentalPlatformAddress = await deployed.address;
+
+  let config = `export const CarRentalPlatformAddress = "${CarRentalPlatformAddress}";`;
+  let data = JSON.stringify(config);
+  fs.writeFileSync('config.js', JSON.parse(data));
+}
--- truffle-config.js
+++ truffle-config.js
@@ -24,58 +24,59 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-module.exports = {  /**
+module.exports = {
+	/**
 
   * contracts_build_directory tells Truffle where to store compiled contracts
   */
-  contracts_build_directory: './build/local-contracts',
+	contracts_build_directory: "./build/bsc-contracts",
 
-  /**
-  * contracts_directory tells Truffle where the contracts you want to compile are located
-  */
-  contracts_directory: './contracts/local-dev',
+	/**
+	 * contracts_directory tells Truffle where the contracts you want to compile are located
+	 */
+	contracts_directory: "./contracts/bsc",
 
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+	/**
+	 * Networks define how you connect to your ethereum client and let you set the
+	 * defaults web3 uses to send transactions. If you don't specify one truffle
+	 * will spin up a development blockchain for you on port 9545 when you
+	 * run `develop` or `test`. You can ask a truffle command to use a specific
+	 * network from the command line, e.g
+	 *
+	 * $ truffle test --network <network-name>
+	 */
 
-   networks: {
-     development: {
-       host: "127.0.0.1",     // Localhost (default: none)
-       port: 8545,            // Standard Ethereum port (default: none)
-       network_id: "*",       // Any network (default: none)
-     }
-   },
+	networks: {
+		development: {
+			host: "127.0.0.1", // Localhost (default: none)
+			port: 8545, // Standard Ethereum port (default: none)
+			network_id: "*", // Any network (default: none)
+		},
+	},
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		// timeout: 100000
+	},
 
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.4",       // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
-  },
+	// Configure your compilers
+	compilers: {
+		solc: {
+			version: "0.8.18", // Fetch exact version from solc-bin (default: truffle's version)
+			// docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+			// settings: {          // See the solidity docs for advice about optimization and evmVersion
+			//  optimizer: {
+			//    enabled: false,
+			//    runs: 200
+			//  },
+			//  evmVersion: "byzantium"
+			// }
+		},
+	},
 
-  // Truffle DB is enabled in this project by default. Enabling Truffle DB surfaces access to the @truffle/db package
-  // for querying data about the contracts, deployments, and networks in this project
-  db: {
-    enabled: true
-  }
+	// Truffle DB is enabled in this project by default. Enabling Truffle DB surfaces access to the @truffle/db package
+	// for querying data about the contracts, deployments, and networks in this project
+	db: {
+		enabled: true,
+	},
 };
